d0bd7d8c1823de11c5d434ef552df25af86e4724
Jennifer Flores <jeflores@mills.edu>
2022-09-23 10:33:45 -0700

    Fix set up

--- src/App.js
+++ src/App.js
@@ -1,106 +1,149 @@
-import React from 'react'
-import Table, { SelectColumnFilter } from './Table'
+import React from "react";
+import Table,{SelectColumnFilter} from "./Table";
 
+const getData = () => [
 
-const getData = () => { 
-  const data = [
-    {
-      name: 'Lydia Farag',
-      email: 'farag.lydia@gmail.com',
-      title: 'MLH OS Fellow @ MSKCC',
-      department: 'Optimization',
-      status: 'Active',
-      role: 'Admin',
-      age: 20,
-      linkedin: https://www.linkedin.com/messaging/thread/2-MjcwNTg2MWQtYjliMy00NTY0LWE0ZmEtODlkNGE5ZTk5MzkyXzAxMA==/
-    }, 
-    
-    {
-      name: 'Jennifer Flores',
-      email: '',
-      title: 'MLH OS Fellow @ MSKCC',
-      department: 'Optimization',
-      status: 'Active',
-      role: 'Admin',
-      age: 20,
-      linkedin: 
-    }, 
-      {
-      name: 'Christian Petricaa',
-      email: 'petricca@gmail.com',
-      title: 'Person',
-      department: 'Student ',
-      status: 'Active',
-      role: 'Admin',
-      age: 20,
-      linkedin: https:/
-    }, 
-    {
-      name: '',
-      email: 'lyds@gmail.com',
-      title: 'MLH',
-      department: 'Optimization',
-      status: 'Active',
-      role: 'Admin',
-      age: 20,
-      linkedin: 
+  {
+    name: "Jane",
+    middle:"",
+    lastname:"Cooper",
+    sex: "Female",
+    age:"14",
+    email: "j.cooper@example.com",
+    phonenumber: "(121)919-3458",
+    department: "Optimization",
+    status: "Active",
+    role: "Patient",
+  },
+  {
+    name: "Cody",
+    middle:"Lee",
+    lastname:"Fisher",
+    sex: "Male",
+    age:"19",
+    email: "cody.fisher@example.com",
+    phonenumber: "(412)347-6292",
+    department: "Intranet",
+    status: "Active",
+    role: "Patient",
+  },
+  {
+    name: "Esther",
+    middle:"Lindel",
+    lastname:"Howard",
+    sex: "Female",
+    age:"21",
+    email: "esther.howard@example.com",
+    phonenumber: "(210)182-1437",
+    department: "Directives",
+    status: "Active",
+    role: "Patient",
+  },
+  {
+    name: "Jenna",
+    middle:"Bert",
+    lastname:"Wilson",
+    sex: "Female",
+    age:"12",
+    email: "jenna_b_w.wilson@example.com",
+    phonenumber: "(919)231-4378",
+    department: "Program",
+    status: "Active",
+    role: "Doctor",
+  },
+  {
+    name: "Kris",
+    middle:"",
+    lastname:"Watson",
+    sex: "Male",
+    age:"32",
+    email: "Wats_kris.watson@example.com",
+    phonenumber: "(919)231-4377",
+    department: "Mobility",
+    status: "Active",
+    role: "Admin",
     },
-   ]
-  return [...data, ...data, ...data]
-}
+  {
+    name: "Cameron",
+    middle:"",
+    lastname:"Williamson",
+    sex: "Male",
+    age:"50",
+    email: "cameron.williamson@example.com",
+    phonenumber: "(510)331-2540",
+    department: "Security",
+    status: "Active",
+    role: "Patient",
+  },
+  {
+    name: "Camille",
+    middle:"Amanda",
+    lastname:"Williamson",
+    sex: "Female",
+    age:"43",
+    email: "cam.williamson@example.com",
+    phonenumber: "(510)332-7640",
+    department: "Security",
+    status: "Inactive",
+    role: "Patient",
+  },
+  
+];
+
+
+
+
 
 function App() {
 
-  const columns = React.useMemo(() => [
+ const columns = React.useMemo(() => [
     {
-      Header: "Name",
-      accessor: 'name',
+      Header: " First Name",
+      accessor: "name",
     },
     {
-      Header: "Title",
-      accessor: 'title',
+      Header: " Middle Name",
+      accessor: "middle",
     },
     {
-      Header: "Status",
-      accessor: 'status',
+      Header: " Last Name",
+      accessor: "lastname",
+    },
+    {
+      Header: "Gender",
+      accessor: "sex",
     },
     {
       Header: "Age",
-      accessor: 'age',
+      accessor: "age",
+    },
+    {
+      Header: "Contact Number",
+      accessor: "phonenumber",
+    },
+    {
+      Header: "Status",
+      accessor: "status",
     },
     {
       Header: "Role",
-      accessor: 'role',
-      Filter: SelectColumnFilter,  // new
-      filter: 'includes',
+      accessor: "role",
+      Filter:SelectColumnFilter,
+      filter:'includes',
     },
-  ], [])
-  
-  
-  const data = React.useMemo(() => getData(), [])
-  
-  
+
+  ],[]);
+
+ const data = React.useMemo(() => getData(), []);
+
   return (
     <>
-      <h1>Hello React!</h1>
+      <h1> Hello React!</h1>
       <div>
-        <Table columns={columns} data={data} />
+        <Table columns= {columns} data={data} />
       </div>
     </>
   );
 }
 
 export default App;
-    
-    
-
-
-
-
-
-
-
-
-
-      
-      
